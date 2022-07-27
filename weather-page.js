8d0fabface3a1577709a5569877de2c029a66c56
edablg <19SOFT1022@isik.edu.tr>
2022-07-27 11:18:46 +0300

    Filter search added

--- weather-page.js
+++ weather-page.js
@@ -115,3 +115,24 @@ function formatDate(date) {
 }
 
 
+const searchFunction = () => {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchInput");
+  filter = input.value;
+  table = document.getElementById("tableId");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    td1 = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    td3 = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      if ((td.innerHTML.indexOf(filter) > -1) || (td1.innerHTML.indexOf(filter) > -1) || (td2.innerHTML.indexOf(filter) > -1) || (td3.innerHTML.indexOf(filter) > -1)) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
